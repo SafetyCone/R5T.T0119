36a746b34d40f116826d6bfbf035e727c9fcf6be
David Coats <david@getsafetycone.com>
2022-02-14 11:17:46 -0800

    Add IAssertion extensions for throws-exception behavior.

--- source/R5T.T0119.X001/Code/Bases/Extensions/IAssertionExtensions.cs
+++ source/R5T.T0119.X001/Code/Bases/Extensions/IAssertionExtensions.cs
@@ -10,6 +10,96 @@ namespace System
 {
     public static class IAssertionExtensions
     {
+        public static void AreEqual<T>(this IAssertion _,
+            T value1,
+            T value2)
+        {
+            Assert.AreEqual(value1, value2);
+        }
+
+        /// <summary>
+        /// Allows explicitly stating that an action should not thrown an exception.
+        /// With MSTest, if code throws an exception the test will fail. However, relying on this behavior means that test code will lack an assertion, which makes these test look different from all other tests.
+        /// This method allows a conforming assertion to be made in the case of testing for no exceptions.
+        /// </summary>
+        /// <remarks>
+        /// From: https://stackoverflow.com/a/9417242
+        /// </remarks>
+        public static async Task DoesNotThrowExceptionAsync(this IAssertion _,
+            Func<Task> action)
+        {
+            try
+            {
+                await action();
+            }
+            catch (Exception exception)
+            {
+                Assert.Fail($"Expected no exception, found:\n{exception.Message}");
+            }
+        }
+
+        public static void IsNull(this IAssertion _,
+            object value)
+        {
+            Assert.IsNull(value);
+        }
+
+        public static void ThrowsException<TException>(this IAssertion _,
+            Action action)
+            where TException : Exception
+        {
+            Assert.ThrowsException<TException>(action);
+        }
+
+        public static void ThrowsException<TException>(this IAssertion _,
+            Action action,
+            string expectedMessageOfException)
+            where TException : Exception
+        {
+            var exceptionWasThrown = false;
+            var exceptionMessageWasAsExpected = false;
+
+            try
+            {
+                action();
+            }
+            catch (TException ex)
+            {
+                exceptionWasThrown = true;
+
+                exceptionMessageWasAsExpected = ex.Message == expectedMessageOfException;
+            }
+
+            if (!exceptionWasThrown)
+            {
+                Assert.Fail($"Expected an exception, but no exception occurred.");
+            }
+
+            if (!exceptionMessageWasAsExpected)
+            {
+                Assert.Fail($"Exception message was not as expected.\nExpected:{expectedMessageOfException}");
+            }
+        }
+
+        public static async Task ThrowsExceptionAsync(this IAssertion _,
+            Func<Task> action)
+        {
+            var exceptionWasThrown = false;
+            try
+            {
+                await action();
+            }
+            catch
+            {
+                exceptionWasThrown = true;
+            }
+
+            if(!exceptionWasThrown)
+            {
+                Assert.Fail($"Expected an exception, but no exception occurred.");
+            }
+        }
+
         public static async Task ThrowsExceptionAsync<TException>(this IAssertion _,
             Func<Task> action)
             where TException : Exception
